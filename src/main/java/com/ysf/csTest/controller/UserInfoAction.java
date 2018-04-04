07975e723f4ac224ba0e03f8ea483f7857ee9505
yangshunfeng <972536780@qq.com>
2018-04-04 10:41:42 +0800

    初始化

---
+++ src/main/java/com/ysf/csTest/controller/UserInfoAction.java
@@ -0,0 +1,14 @@
+package com.ysf.csTest.controller;
+
+import com.ysf.csTest.controller.common.BaseAction;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by 972536780 on 2018/4/1.
+ */
+@RestController
+@RequestMapping("/user")
+public class UserInfoAction extends BaseAction {
+
+}
